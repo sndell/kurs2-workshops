6aa53999dfef9dac617b777e083ccbd17781c2a2
Benjamin Sundell <sundell.benjamin@gmail.com>
2022-11-14 02:51:16 +0100

    changes

--- workshop5/2/index.js
+++
@@ -1,7 +0,0 @@
-const modal = document.getElementById('modal');
-
-const toggleModal = (value) => {
-  console.log(value);
-  if (value) modal.classList.remove('hidden');
-  else modal.classList.add('hidden');
-};
